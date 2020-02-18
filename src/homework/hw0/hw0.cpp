bd74cdb471b9553e4794800dc7dd4e68b4fbaf71
Yashasvi Sriram <yash.3997@gmail.com>
2020-02-17 18:53:58 -0600

    completes hw0

--- src/homework/hw0/hw0.cpp
+++ src/homework/hw0/hw0.cpp
@@ -6,12 +6,11 @@ std::vector<Eigen::VectorXd> DrawEllipse(Eigen::Vector2d center,
   std::vector<Eigen::VectorXd> points(n_pts);
 
   for (size_t i = 0; i < n_pts; ++i) {
-    // n_pts-1 so that the first and last points are the same (completing the
-    // circle)
+    // n_pts-1 so that the first and last points are the same (completing the circle)
     double theta = 6.28318530718 * i / (n_pts - 1);
 
     // HW0: Modify this to account for the center, major axis, and minor axis
-    points[i] = Eigen::Vector2d(std::cos(theta), std::sin(theta));
+    points[i] = Eigen::Vector2d(center + major_axis * std::cos(theta) + minor_axis * std::sin(theta));
   }
 
   return points;
