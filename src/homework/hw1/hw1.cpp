497fc643c148f26afe4b97dd2a00f51bac8e150d
Yashasvi Sriram <yash.3997@gmail.com>
2020-02-22 15:58:31 -0600

    impls NonLinearLSSolve() with some fixmes

--- src/homework/hw1/hw1.cpp
+++ src/homework/hw1/hw1.cpp
@@ -28,8 +28,13 @@ LinearMLESolve(vector<double> d_Lis, // vector of distance projected onto each l
     if (A.determinant() < 1e-6) {
         cerr << "Singular matrix formed. Cannot estimate position of robot" << endl;
     }
-    Matrix<double , 2, 1> G_P_R = A.inverse() * b;
+    Matrix<double, 2, 1> G_P_R = A.inverse() * b;
     return G_P_R;
+}
+
+double euclideanDistance(VectorXd p1, VectorXd p2) {
+    VectorXd dist = p1 - p2;
+    return dist.norm();
 }
 
 VectorXd
@@ -41,5 +46,30 @@ NonLinearLSSolve(vector<double> dists, // vector of distance to each landmark
                  size_t n_iter)  // Number of iterations of Gauss-Newton to run
 {
     // TODO: Use Gauss-Newton to iteratively solve the Weighted Non-Linear Least Squares Problem
-    return G_p_R_0;
+    assert(dists.size() == sigmas.size() && sigmas.size() == G_p_Lis.size());
+    VectorXd currentGuess = G_p_R_0;
+
+    for (int iter = 0; iter < n_iter; ++iter) {
+        Matrix<double, 2, 2> A;
+        A << 0, 0, 0, 0;
+        Matrix<double, 2, 1> b;
+        b << 0, 0;
+        // fixme: is alpha and n_iter tuned ?
+        for (int i = 0; i < G_p_Lis.size(); ++i) {
+            double h_i_currentGuess = euclideanDistance(currentGuess, G_p_Lis[i]);
+            VectorXd H_i_T = (currentGuess - G_p_Lis[i]) * (1 / h_i_currentGuess);
+            double sigma_i = sigmas[i];
+            double r_i = dists[i] - h_i_currentGuess;
+            Matrix<double, 2, 2> Ai = H_i_T * (1 / sigma_i) * H_i_T.transpose();
+            Matrix<double, 2, 1> bi = H_i_T * (1 / sigma_i) * r_i; // fixme: no sigma here ?
+            A += Ai;
+            b += bi;
+        }
+        if (A.determinant() < 1e-6) {
+            cerr << "Singular matrix formed. Cannot estimate position of robot" << endl;
+        }
+        VectorXd change = A.inverse() * b;
+        currentGuess = currentGuess + alpha * change;
+    }
+    return currentGuess;
 }
