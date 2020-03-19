aa5ab7e88f597a198b35c4d0d0a01c6a56b65e0b
Bobby Davis <davi1510@umn.edu>
2020-03-19 16:13:55 -0500

    Adding Gnuplot backend

--- src/robot_simulation/test_simulator.cpp
+++ src/robot_simulation/test_simulator.cpp
@@ -1,56 +1,73 @@
 #include <robot_simulation/simulator.h>
-#include <visualization/vis.h>
+#include <visualization/gvis.h>
 
 int main() {
   std::string scene_name = "simple_rectangle.scn";
 
   Simulator sim(scene_name);
-  Visualizer vis;
+  GVisualizer vis;
 
   size_t scan_fig_id = vis.AddFigure("Scan", true);
+  vis.ShowFigures();
+  // Wait a second to initialize everything in the simulator
+  vis.PauseFigures(1);
 
-  for (auto& line : sim.GetLines()) {
-    vis.Plot(scan_fig_id, line, "b-");
-  }
 
   Eigen::Vector2d ctrl(0.3,0.5);
   sim.SendControl(ctrl);
 
   std::vector<Eigen::VectorXd> poses;
   for (size_t i = 0; i < 1000; ++i) {
+    // Check the sensors to see if there's new information
+    OdometryData odom = sim.GetOdometry();
+    LaserScanData scan = sim.GetLaserScan();
+
+    // ------------------
+    //    Drawing Code
+    // ------------------
+    auto t_start = std::chrono::high_resolution_clock::now();
+    // For visualization purposes only, you shouldn't use this in your localization
     poses.push_back(sim.GetTruePose());
+
+    // Clear all old lines
+    vis.ClearFigure(scan_fig_id);
+
+    // Draw the building
+    for (auto& line : sim.GetLines()) {
+      vis.Plot(scan_fig_id, line, "blue");
+    }
+
+    // Draw the Laser scan:
+    std::vector<Eigen::VectorXd> line;
+    Eigen::Vector2d base = poses.back().head<2>() - 0.5 * sim.robot_radius * Eigen::Vector2d(std::cos(poses.back()[2]), std::sin(poses.back()[2]));
+    line.push_back(base);
+    for (size_t i = 0; i < sim.n_lasers; ++i) {
+      if (scan.scan[i] == sim.laser_dist_overdist_val) {
+        continue;
+      }
+      double perc = static_cast<double>(i) / (static_cast<double>(sim.n_lasers) - 1.0);
+      double th = sim.min_th + (sim.max_th - sim.min_th) * perc;
+
+      Eigen::VectorXd scan_pt_i = base + scan.scan[i] * Eigen::Vector2d(std::cos(th+poses.back()[2]), std::sin(th+poses.back()[2]));
+
+      line.push_back(scan_pt_i);
+      vis.Plot(scan_fig_id, line, "red");
+      line.pop_back();
+    }
+
+    // Update the figure
+    vis.DrawFigure(scan_fig_id);
+    vis.SetFigureAxisLimits(scan_fig_id, Eigen::Vector2d(poses.back()[0]-10,poses.back()[0]+10), Eigen::Vector2d(poses.back()[1]-10,poses.back()[1]+10));
+
+    auto t_now = std::chrono::high_resolution_clock::now();
+    auto iter_dur = std::chrono::duration_cast<std::chrono::nanoseconds>(t_now - t_start).count();
+    float iter_dur_ms = static_cast<float>(iter_dur) / 1e6;
+    std::cout << "Drawing takes: " << iter_dur_ms << "ms" << std::endl;
+
+    // Sleep for 10 milliseconds before running again
     std::this_thread::sleep_for(std::chrono::milliseconds(10));
   }
 
   sim.SendControl(Eigen::Vector2d::Zero());
-
-  for (size_t i = 0; i < 100; ++i) {
-    poses.push_back(sim.GetTruePose());
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
-  }
-  vis.Plot(scan_fig_id, poses, "g-x");
-
-
-  LaserScanData scan = sim.GetLaserScan();
-
-  std::vector<Eigen::VectorXd> line;
-  line.push_back(Eigen::Vector2d::Zero());
-  for (size_t i = 0; i < sim.n_lasers; ++i) {
-    continue;
-    if (scan.scan[i] == sim.laser_dist_overdist_val) {
-      continue;
-    }
-    double perc = static_cast<double>(i) / (static_cast<double>(sim.n_lasers) - 1.0);
-    double th = sim.min_th + (sim.max_th - sim.min_th) * perc;
-
-    Eigen::VectorXd scan_pt_i = scan.scan[i] * Eigen::Vector2d(std::cos(th), std::sin(th));
-
-    line.push_back(scan_pt_i);
-
-    vis.Plot(scan_fig_id, line, "r-");
-
-    line.pop_back();
-  }
-  vis.ShowFigures();
   vis.PauseFigures(-1);
 }
---
+++ src/visualization/gvis.cpp
@@ -0,0 +1,71 @@
+#include <visualization/gvis.h>
+
+#include <iostream>
+#include <string>
+#include <thread>
+#include <chrono>
+
+GVisualizer::GVisualizer() {
+  
+}
+
+size_t GVisualizer::AddFigure(std::string figure_title, bool force_square) {
+  plots.emplace_back("lines");
+  plots.back().set_title(figure_title);
+  if (force_square) {
+    plots.back().set_xautoscale();
+    plots.back().set_yautoscale();
+  }
+  return plots.size() - 1;
+}
+
+void GVisualizer::ShowFigures() {
+  for (auto& plot : plots) {
+    plot.showonscreen();
+  }
+}
+
+void GVisualizer::PauseFigures(double pause_time_s) {
+  if (pause_time_s < 0) {
+    for (auto& plot : plots) {
+      if (plot.is_valid()) {
+        plot << "pause mouse close";
+      }
+    }
+  } else {
+    std::this_thread::sleep_for(std::chrono::nanoseconds(static_cast<int>(pause_time_s * 1e9)));
+  }
+}
+
+void GVisualizer::Plot(size_t fig_id, const std::vector<Eigen::VectorXd> &points,
+                      std::string color) {
+  double x, x_prev = points[0][0], y, y_prev = points[0][1];
+  for (size_t i = 0; i < points.size(); ++i) {
+    x = points[i][0];
+    y = points[i][1];
+
+    plots[fig_id] << "set arrow from " + std::to_string(x_prev) + "," + std::to_string(y_prev) + " to " + std::to_string(x) + "," + std::to_string(y) + " nohead lc rgb \""+color+"\"\r\n";
+
+    x_prev = x;
+    y_prev = y;
+  }
+}
+
+void GVisualizer::SetFigureAxisLimits(size_t fig_id,
+                                     const Eigen::Vector2d &x_lim,
+                                     const Eigen::Vector2d &y_lim) {
+  plots[fig_id].set_xrange(x_lim[0], x_lim[1]);
+  plots[fig_id].set_yrange(y_lim[0], y_lim[1]);
+}
+
+void GVisualizer::ClearFigure(size_t fig_id) {
+  plots[fig_id].reset_plot();
+  plots[fig_id].cmd("unset arrow");
+}
+
+void GVisualizer::DrawFigure(size_t fig_id) {
+  plots[fig_id].cmd("plot 0 with points pc rgb \"white\" ");
+}
+
+GVisualizer::~GVisualizer() {
+}
